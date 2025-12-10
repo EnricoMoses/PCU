121efbeb4b0b5cbec409d50622087b325171755d
EnricoMoses <enricomoseswijaya@gmail.com>
2025-12-10 20:16:51 +0700

    belajar uas

--- Tutor/11-10/cobaa.py
+++ Tutor/11-10/cobaa.py
@@ -1,8 +1,11 @@
-a = [1,2,3,4,5]
-b = [6,7,8,9,10]
+# a = [1,2,3,4,5]
+# b = [6,7,8,9,10]
+#
+# import numpy as np
+# a = np.array(a)
+# b = np.array(b)
+# print(a)
+# print(b)
 
-import numpy as np
-a = np.array(a)
-b = np.array(b)
-print(a)
-print(b)
+URUTAN_ANGKA  = {str(i): i-1 for i in range(1, 10)}
+print(URUTAN_ANGKA)
--- Tutor/11-10/no_1_uas_2024.py
+++ Tutor/11-10/no_1_uas_2024.py
@@ -1,3 +1,8 @@
+urutan_angka = {str(i): i-1 for i in range(1, 10)}
+urutan_angka['0'] = 9
+
+urutan_simbol = {'K': 0, 'M':1, 'B':2, 'S':3}
+
 def minta_jumlah():
     while True:
         try:
@@ -22,10 +27,39 @@ def minta_kartu(n):
     line = input('Kartu (dipisah dengan spasi): ')
     tokens = [t.upper() for t in line.split()]
     if len(tokens) != n or any(not valid_kartu(t) for t in tokens):
-        print('Mohon ulangi penginputan kartu!.')
+        print('Mohon ulangi penginputan kartu!')
         return minta_kartu(n)
     return tokens
 
+def to_tuple_list(tokens):
+    return [(t[0], t[1], t) for t in tokens]
+
+def card_sorting(cards_tuples, hasil):
+    if len(cards_tuples) == 0:
+        return hasil
+
+    highest = cards_tuples[0]
+
+    for r, s, kode in cards_tuples[1:]:
+        if urutan_angka[r] > urutan_angka[highest[0]]:
+            highest = (r, s, kode)
+        elif urutan_angka[r] == urutan_angka[highest[0]]:
+            if urutan_simbol[s] > urutan_simbol[highest[1]]:
+                highest = (r, s, kode)
+
+
+    idx = cards_tuples.index(highest)
+    cards_tuples.pop(idx)
+    hasil.append(highest[2])
+
+    return card_sorting(cards_tuples, hasil)
 
 def main():
     n = minta_jumlah()
+    tokens = minta_kartu(n)
+    cards_tuples = to_tuple_list(tokens)
+    hasil = card_sorting(cards_tuples, [])
+    print('Hasil pengurutan:', ' '.join(hasil))
+
+
+main()
