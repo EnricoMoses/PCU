4709bb77f146172bc3845b0ae451898bc1f8a8e5
EnricoMoses <enricomoseswijaya@gmail.com>
2026-03-12 22:35:38 +0700

    interview epiclair

--- Tutor/12-10/tes.py
+++ Tutor/12-10/tes.py
@@ -34,4 +34,40 @@ print(x)
 print((()))
 
 print('halo teman teman')
+print('saklhflaskhf')# reference
+
+x = 5
+y = x
+y = 7
+print(x)
+print((()))
+
+print('halo teman teman')
+print('saklhflaskhf')# reference
+
+x = 5
+y = x
+y = 7
+print(x)
+print((()))
+
+print('halo teman teman')
+print('saklhflaskhf')# reference
+
+x = 5
+y = x
+y = 7
+print(x)
+print((()))
+
+print('halo teman teman')
+print('saklhflaskhf')# reference
+
+x = 5
+y = x
+y = 7
+print(x)
+print((()))
+
+print('halo teman teman')
 print('saklhflaskhf')
